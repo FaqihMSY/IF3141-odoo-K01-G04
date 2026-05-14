ce8d3ba950521e065a618eebb68379b4949f5f82
Muhammad Fithra Rizki <167663689+fithrarzk@users.noreply.github.com>
2026-05-14 19:42:23 +0700

    refactor: move purchase code to new folder

--- custom_addons/raw_material_inventory/models/__init__.py
+++ custom_addons/raw_material_inventory/models/__init__.py
@@ -1,6 +1,4 @@
 from . import product_template
-from . import res_partner
-from . import purchase_order
 from . import raw_material_receipt
 from . import raw_material_usage
 from . import raw_material_stock_report
